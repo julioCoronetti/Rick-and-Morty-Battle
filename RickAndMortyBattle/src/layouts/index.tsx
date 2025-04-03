34b97b5fafcc5f6f65f3f212ad275df781df95e5
Julio Coronetti <timeset13@Outlook.com>
2025-04-03 19:33:23 -0300

    fix: restore Footer component in Home page and DefaultLayout for consistent layout

--- RickAndMortyBattle/src/layouts/index.tsx
+++ RickAndMortyBattle/src/layouts/index.tsx
@@ -1,14 +1,12 @@
 import { Outlet } from "react-router-dom"
 import { Navigation } from "../components/Navigation"
 import { LayoutContainer } from "./styles"
-import { Footer } from "../components/Footer"
 
 export const DefaultLayout = () => {
     return (
         <LayoutContainer>
             <Navigation />
             <Outlet />
-            <Footer />
         </LayoutContainer>
     )
 }
