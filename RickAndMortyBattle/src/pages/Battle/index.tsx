4027f76a321e6fc815dcb5e172ba85e44b13412c
Julio Coronetti <timeset13@Outlook.com>
2025-04-03 21:15:56 -0300

    feat: implement character fetching in Battle page; add loading state and display character details

--- RickAndMortyBattle/src/pages/Battle/index.tsx
+++ RickAndMortyBattle/src/pages/Battle/index.tsx
@@ -1,9 +1,44 @@
+import { useEffect, useState } from "react";
 import { HomeButton } from "../../components/HomeButton"
 import { BattleContainer } from "./styles"
 
+interface Character {
+    id: number;
+    name: string;
+    image: string;
+}
+
 export const Battle = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [characters, setCharacters] = useState<Character[]>([]);
+    const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+    const fetchData = async () => {
+        setIsLoading(true);
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${randomNumber}`);
+        const data = await response.json();
+        setCharacters(data.results.slice(0, 3));
+        setIsLoading(false);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
     return (
         <BattleContainer>
+            {isLoading ? (
+                <p>Carregando...</p>
+            ) : (
+                characters.map((character) => (
+                    <div key={character.id}>
+                        <img src={character.image} alt={character.name} />
+                        <p>{character.name}</p>
+                    </div>
+                ))
+            )}
+
             <HomeButton />
         </BattleContainer>
     )
--- RickAndMortyBattle/src/pages/Battle/styles.ts
+++ RickAndMortyBattle/src/pages/Battle/styles.ts
@@ -4,4 +4,29 @@ export const BattleContainer = styled.section`
     width: 100%;
     height: 100vh;
     
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    div {
+        width: 150px;
+        height: 150px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1rem;
+
+        img {
+            width: 100px;
+            height: 100px;
+        }
+
+        p {
+            text-align: center;
+            width: 100px;
+        }
+    }
+
+    
 `;
