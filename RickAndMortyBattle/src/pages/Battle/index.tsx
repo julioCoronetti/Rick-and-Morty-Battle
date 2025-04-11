82a738a1090596cae7a9a33eb600dbabf7586dd1
Julio Coronetti <timeset13@Outlook.com>
2025-04-10 22:39:42 -0300

    feat: simplify Battle component by removing loading state and character fetching; utilize useUser context for character management

--- RickAndMortyBattle/src/pages/Battle/index.tsx
+++ RickAndMortyBattle/src/pages/Battle/index.tsx
@@ -1,39 +1,22 @@
 import { useEffect, useState } from "react";
 import { HomeButton } from "../../components/HomeButton"
 import { BattleContainer } from "./styles"
-import { Character } from "../../contexts/UserProvider";
+import { useUser } from "../../contexts/UserProvider";
+import { useNavigate } from "react-router-dom";
 
 export const Battle = () => {
-    const [isLoading, setIsLoading] = useState<boolean>(true);
-    const [characters, setCharacters] = useState<Character[]>([]);
-    const randomNumber = Math.floor(Math.random() * 3) + 1;
-
-    const fetchData = async () => {
-        setIsLoading(true);
-        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${randomNumber}`);
-        const data = await response.json();
-        setCharacters(data.results.slice(0, 3));
-        setIsLoading(false);
-    };
+    const { characters } = useUser();
+    const navigate = useNavigate();
 
     useEffect(() => {
-        fetchData();
-    }, []);
-
+        if (characters.length === 0) {
+            alert('Você precisa montar seu time antes de iniciar a batalha!');
+            navigate('/teams'); // ou a página que faz sentido pro seu fluxo
+        }
+    }, [characters, navigate]);
 
     return (
         <BattleContainer>
-            {isLoading ? (
-                <p>Carregando...</p>
-            ) : (
-                characters.map((character) => (
-                    <div key={character.id}>
-                        <img src={character.image} alt={character.name} />
-                        <p>{character.name}</p>
-                    </div>
-                ))
-            )}
-
             <HomeButton />
         </BattleContainer>
     )
